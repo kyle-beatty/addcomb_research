b4979d365a596ffcbdc927fead66cbfabcdbdd13
Kyle Beatty <w.kyle.beatty@protonmail.com>
2022-06-26 21:43:22 -0400

    Update Summer 2022

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,131 @@
 pub mod spanning {
     use addcomb_comp::setlike::SetLike;
-    use addcomb_comp::setlike::Group;
     use addcomb_comp::exactset;
     use addcomb_comp::comb::gcd;
 
     use std::rc::Rc;
     use addcomb_comp::exactset::GElem;
 
+    pub fn do_two_elements_span(k: u32, s: u32) -> Option<Vec<GElem>> {
+        let g = Rc::new(vec![2, 2 * k]);
+        let g_size = 4 * k;
+
+        // Haesoo's conjecture proven!
+        if g_size > 2*s*s {
+            return None;
+        }
+
+        for pair in <Vec<GElem> as SetLike>::each_set_exact(g.clone(), 2) {
+            // Filter out "upper half"
+            if (pair[0].0[1] > k) || (pair[1].0[1] > k) {
+                //println!("{}, {}: {}", pair[0].0[1], pair[1].0[1], k);
+                continue;
+            }
+
+            if does_pair_span(k, s, &g, &pair) {
+                return Some(pair);
+            }
+        }
+
+        None
+    }
+
+    // Make sure we pass it a pair of spanning elements
+    pub fn does_pair_span(k: u32, s: u32, g: &Rc<Vec<u32>>, pair: &Vec<GElem>) -> bool {
+        // Puts the pair into form {(c, x), (d, y)}
+        let p = &pair[0];
+        let q = &pair[1];
+        let c = p.0[0];
+        let x = p.0[1];
+        let d = q.0[0];
+        let y = q.0[1];
+
+        // Optimization 1 + bonus:
+        // Proposition 10 of Park's Conjecture proof paper
+        if c == 0 {
+            if (d == 0) || /* Prop 10 : */ (x % 2 == 0) {
+                return false;
+            }
+        } else if (d == 0) && (y % 2 == 0) { /* more prop 10*/
+            return false;
+        }
+
+        // Optimization 2
+        if gcd(2*k, gcd(x, y)) != 1 {
+            return false;
+        }
+
+        // Optimization 3
+        // Compute if x and y are not in [k / s, -k / s]
+        let lb = k / s;
+        let ub = 2*k - (k / s);
+        if (x < lb || x > ub) && (y < lb || y > ub) {
+            return false;
+        }
+
+        // Finally, check span
+        // WARNING this should be the same as the size of g, but if somebody
+        // passes this the wrong stuff then we're screwed
+        let g_size = 4 * k;
+
+        let span = exactset::hfold_interval_signed_sumset(&pair, (0, s), g.clone());
+        let size = span.len() as u32;
+
+        size == g_size
+    }
+
+    pub fn print_spanning_pairs(k: u32, s: u32) {
+        let g = Rc::new(vec![2, 2 * k]);
+
+        println!("Printing spanning pairs for (s, k) = ({}, {}) :", s, k);
+
+        // WE'VE PROVEN HAESOO'S CONJECTURE!!!
+        if 4*k > 2*s*s {
+            return;
+        }
+
+        for pair in <Vec<GElem> as SetLike>::each_set_exact(g.clone(), 2) {
+            let p = &pair[0];
+            let q = &pair[1];
+            let x = p.0[1];
+            let y = q.0[1];
+
+            // Optimization 4
+            // Ignoring the "upper half" of potential spanning sets
+            // NOTE only one not included in does_pair_span below
+            if x > k || y > k {
+                continue;
+            }
+
+
+            if does_pair_span(k, s, &g, &pair) {
+                println!("{}: {:?}", k, pair);
+                continue;
+            }
+        }
+    }
+
+    pub fn print_spannable(s : u32) {
+        println!("Spannable groups for s = {}:", s);
+        
+        println!("  k < s given by proposition B.55");
+
+        // This range excludes the maximum k, since we already know it by Park's
+        for k in s..(s*s / 2) {
+            match do_two_elements_span(k, s) {
+                Some(pair) => {
+                    println!("  k = {:>3}  {:?}", k, pair);
+                }
+                None => {
+                    println!("  k = {:>3}  no spanning pair", k);
+                }
+            }
+        }
+
+        println!("  k = s^2 / 2 given by Park's\n");
+    }
+
+    // NOTE OLD FUNCTION
     // REMINDER this is for use with k above Haesoo's conjectured upper limit
     // We've modified this so much that it excludes valid spanning sets for lower k
     pub fn print_spans(s: u32, k: u32) {
@@ -109,63 +228,4 @@ pub mod spanning {
             println!();
         }
     }
-
-    pub fn print_elements_span(k: u32, s: u32) -> Option<Vec<GElem>> {
-        let g = Rc::new(vec![2, 2 * k]);
-        let g_size = g.gsize();
-
-        // WE'VE PROVEN HAESOO'S CONJECTURE!!!
-        if 4*k > 2*s*s {
-            return None;
-        }
-
-        for a in <Vec<GElem> as SetLike>::each_set_exact(g.clone(), 2) {
-            let p = &a[0];
-            let q = &a[1];
-            let c = p.0[0];
-            let x = p.0[1];
-            let d = q.0[0];
-            let y = q.0[1];
-
-            // Optimization 1
-            if c == 0 && d == 0 {
-                continue;
-            }
-
-            // Optimization 4
-            // Ignoring the "upper half" of potential spanning sets
-            if x > k || y > k {
-                continue;
-            }
-
-            // Optimization 2
-            if gcd(2*k, gcd(x, y)) != 1 {
-                continue;
-            }
-
-            // Optimization 3
-            // Compute if x and y are not in [k / s, -k / s]
-            // We've already knocked out any in the upper half
-            let lb = k / s;
-            if x < lb && y < lb {
-                continue;
-            }
-
-            let size =
-                exactset::hfold_interval_signed_sumset(&a, (0, s), g.clone()).len()
-                as u32;
-
-            if size == g_size {
-                //println!("  {}: {:?}", s, a);
-                //TODO remove this
-                println!("{}: {:?}", k, a);
-                return None;
-            }
-        }
-
-        println!("{}: no {}-spanning pair", k, s);
-
-        None
-    }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,31 @@
 use std::env;
-use std::rc::Rc;
 
-use addcomb_comp::exactset;
-use addcomb_comp::exactset::GElem;
-
-use addcomb_research::spanning::print_elements_span;
-
-// Test my conjecture for s odd
-fn test_conj_odd(s : u32) {
-    if s % 2 == 0 {
-        return;
-    }
-
-    // generate the range of k
-    //let start = 3 * s - 3;
-    let start = (s+1) / 2;
-    let end = (s * s + 1) / 2;
-
-    // generate our spanning set for max k, to test it for smaller
-    let x = match s % 4 {
-        1 => (s+1)/2,
-        3 => (s-1)/2,
-        _ => {panic!("Something went terribly wrong with the spanning pair");}
-    };
-
-    let y = match s % 4 {
-        1 => (s-1)/2,
-        3 => (s+1)/2,
-        _ => {panic!("Something went terribly wrong with the spanning pair");}
-    };
-
-    let a_1 = GElem(vec![0, x]);
-    let a_2 = GElem(vec![1, y]);
-    let a = vec![a_1, a_2];
-
-    println!("s = {}, A = {:?}", s, a);
-
-    for k in start..end {
-        let g = Rc::new(vec![2, 2 * k]);
-        let g_size = 4 * k;
-
-        let sumset = exactset::hfold_interval_signed_sumset(&a, (0, s), g.clone());
-        let size = sumset.len() as u32;
-        
-        if size == g_size {
-            println!("  k = {:3}: success", k);
-        } else {
-            println!("X k = {:3}: FAILURE", k);
-        }
-    }
-
-}
-
+use addcomb_research::spanning::*;
 
 fn run(s : u32, k : u32) {
-    print_elements_span(k, s);
+    println!("{:?}", do_two_elements_span(k, s));
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     match args.len() {
-        1 => { run(5, 1) },
+        1 => {
+            for s in (2..13).step_by(2) {
+                print_spannable(s);
+            }
+        },
         2 => {
             let s = args[1].parse().expect("arguments must be integers");
 
-            run(s, 1);
+            //run(s, 1);
+            print_spannable(s);
         },
         3 => {
             let s = args[1].parse().expect("arguments must be integers");
             let k = args[2].parse().expect("arguments must be integers");
 
-            run(s, k);
+            print_spanning_pairs(k, s);
         },
         _ => { println!("Wrong number of arguments given"); },
     }
