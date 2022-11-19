a94b39dfce5e6e9334151c4ef5abedb23c72fbfe
Kyle Beatty <w.kyle.beatty@protonmail.com>
2022-11-19 12:58:50 -0500

    Add specific pair check to junxue

--- src/junxue.rs
+++ src/junxue.rs
@@ -30,8 +30,59 @@ pub fn junxue_main(args : &Vec<String>) {
                 }
             }
         },
+        8 => {
+            let s = args[2].parse().expect("arguments must be integers");
+            let k : u32 = args[3].parse().expect("arguments must be integers");
+
+            let a = args[4].parse().expect("arguments must be integers");
+            let x = args[5].parse().expect("arguments must be integers");
+            let b = args[6].parse().expect("arguments must be integers");
+            let y = args[7].parse().expect("arguments must be integers");
+
+            let g = Rc::new(vec![3, 3 * k]);
+
+            let p = GElem(vec![a, x]);
+            let q = GElem(vec![b, y]);
+            let pair = vec![p, q];
+
+            let span =
+                exactset::hfold_interval_signed_sumset(&pair, (0, s), g.clone());
+
+            let does_it_span = (span.len() as u32) == 9 * k;
+
+            if does_it_span {
+                println!("k ={:>3} The pair {:?} {}-spans Z_2 x Z_{}\n",
+                         k, &pair, s, 2*k);
+            } else {
+                //println!("  {}-span of {:?}: {:?}\n", s, pair, span);
+                println!("k ={:>3} The pair {:?} DOES NOT {}-span Z_2 x Z_{}\n",
+                         k, &pair, s, 2*k);
+            }
+        },
         _ => {
             println!("Junxue main default");
         }
     }
+}
+
+pub fn do_two_elements_span(k: u32, s: u32) -> Option<Vec<GElem>> {
+    let g = Rc::new(vec![3, 3 * k]);
+    let _g_size = 9 * k;
+
+    for pair in <Vec<GElem> as SetLike>::each_set_exact(g.clone(), 2) {
+        if does_pair_span(k, s, &g, &pair) {
+            return Some(pair);
+        }
+    }
+
+    None
+}
+
+pub fn does_pair_span(k: u32, s: u32, g: &Rc<Vec<u32>>, pair: &Vec<GElem>) -> bool {
+    let g_size = 9 * k;
+
+    let span = exactset::hfold_interval_signed_sumset(&pair, (0, s), g.clone());
+    let size = span.len() as u32;
+
+    size >= g_size
 }
